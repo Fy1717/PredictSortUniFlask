ccd368980696c2f3c1aaddf74983c992e86afd69
Furkan Yıldız <frknyldz8489@gmail.com>
2019-06-26 22:09:37 +0300

    Version 2.0 Arayüz Hazırlandı

--- 1main.py
+++ 1main.py
@@ -1,10 +1,98 @@
+
+# -*- coding: utf-8 -*-
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
-dt=pd.read_csv("data.csv")
-print(dt)
-reg = linear_model.Ridge()
-reg.fit(dt[["Konum","AkademikYayinSayisi","AkademikYayinDerecesi","OgrenciSeviyesi","DersSaati","DonemlikStaj"]],dt.sr)
-sonuc=int(reg.predict([[10,40,3,10,35,0]]))
-print("\n TAHMİNİ SIRALAMA :" , sonuc)
+
+
+root = tk.Tk()
+root.title( "Yüksek Öğrenim Sıralama    " )
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+window_width = 720
+window_height = 550
+
+window_position_x = 0
+window_position_y = 0
+root.geometry("310x300")
+
+frame = tk.Frame(root, bg='cyan')
+frame.pack(side="bottom", fill='both', expand='no')
+
+
+
+konumPuanıLabel = Label(root,width=20,height=3,text="KONUM PUANI" , font="Times 10 bold")
+konumPuanıLabel.place(x=30 ,y=26)
+
+akademikYayinSayisiLabel = Label(root,width=20,height=3,text="YAYIN SAYISI" , font="Times 10 bold")
+akademikYayinSayisiLabel.place(x=30,y=66)
+
+yayinDerecesiLabel = Label(root,width=20,height=3,text="YAYIN DERECESİ" , font="Times 10 bold")
+yayinDerecesiLabel.place(x=30 ,y=106)
+
+ogrenciSeviyesiLabel = Label(root,width=20,height=3,text="ÖĞRENCİ SEVİYESİ" , font="Times 10 bold")
+ogrenciSeviyesiLabel.place(x=30,y=146)
+
+dersSaatiLabel = Label(root,width=20,height=3,text="DERS SAATİ " , font="Times 10 bold")
+dersSaatiLabel.place(x=30 ,y=186)
+
+
+
+konumPuanı_edit = Entry(root,width=5)
+konumPuanı_edit.place(x=200,y=40)
+konumPuanı_edit.config(font="bold")
+konumPuanı_edit.insert(END,"")
+
+akademikYayinSayisi_edit = Entry(root,width=5)
+akademikYayinSayisi_edit.place(x=200,y=80)
+akademikYayinSayisi_edit.config(font="bold")
+akademikYayinSayisi_edit.insert(END,"")
+
+yayinDerecesi_edit = Entry(root,width=5)
+yayinDerecesi_edit.place(x=200,y=120)
+yayinDerecesi_edit.config(font="bold")
+yayinDerecesi_edit.insert(END,"")
+
+ogrenciSeviyesi_edit = Entry(root,width=5)
+ogrenciSeviyesi_edit.place(x=200,y=160)
+ogrenciSeviyesi_edit.config(font="bold")
+ogrenciSeviyesi_edit.insert(END,"")
+
+dersSaati_edit = Entry(root,width=5)
+dersSaati_edit.place(x=200,y=200)
+dersSaati_edit.config(font="bold")
+dersSaati_edit.insert(END,"")
+
+
+
+def Hesapla():
+
+    konumPuanı = int(konumPuanı_edit.get())
+    yayinSayisi = int(akademikYayinSayisi_edit.get())
+    yayinDerecesi = int(yayinDerecesi_edit.get())
+    ogrenciSeviyesi = int(ogrenciSeviyesi_edit.get())
+    dersSaati = int(dersSaati_edit.get())
+    
+    dt=pd.read_csv("data.csv")
+    print(dt)
+    reg = linear_model.Ridge()
+    reg.fit(dt[["Konum","AkademikYayinSayisi","AkademikYayinDerecesi","OgrenciSeviyesi","DersSaati"]],dt.sr)
+    sonuc=int(reg.predict([[konumPuanı,yayinSayisi,yayinDerecesi,ogrenciSeviyesi,dersSaati]]))
+    tk.messagebox.showinfo("\n TAHMİNİ SIRALAMA :" , sonuc)
+
+
+
+controlButton = Button(root,text="HESAPLA",width=20,command=Hesapla)
+controlButton.config(font="bold")
+controlButton.place(x=40,y=250)
+
+
+
+root.mainloop()
